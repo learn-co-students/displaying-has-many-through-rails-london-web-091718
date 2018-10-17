d0cd7b53755ceca68111cbb5f7e1a4a338db2284
deedeeh <dinahafez.8@gmail.com>
2018-10-17 13:39:19 +0100

    Done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+post1 = Post.create(title: "Rails", content: "Just a small talk about this topic!")
+post2 = Post.create(title: "Ruby", content: "Just a small talk about this programming language!")
+
+user1 = User.create(username: "deedee", email: "example@me.com")
+user2 = User.create(username: "Lala", email: "hello@alsome.com")
+
+comment1 = Comment.create(content: "It is a great topic!", user: user1 , post: post1)
+comment2 = Comment.create(content: "It is one of my best frameworks", user: user2, post: post1)
+comment3 = Comment.create(content: "An awesome OO language", user: user1, post: post2)
